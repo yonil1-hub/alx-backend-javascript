3253bbbd6e87dfb545d55f7abb5153923c3c11ba
yonil1-hub <wyml@yahoo.com>
2023-05-22 18:55:52 +0300

    stdin and stdout

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  console.log('This important software is now closing');
+  rl.close();
+});
